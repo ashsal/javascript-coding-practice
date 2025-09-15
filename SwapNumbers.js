99a17293bd489b4245b2142b6d3893d30ae5fbac
Ashish Saluja <ashishsaluja1@gmail.com>
2025-09-15 18:23:46 +0400

    Swap number

---
+++ SwapNumbers.js
@@ -0,0 +1,17 @@
+let a = 5;
+let b = 6;
+
+console.log(`a is ${a} and b is ${b}`);
+// Write logic to swap the numbers
+//a = b; // a: 6 , b: 6
+//b = a; //
+
+// let c = a;
+// a = b;
+// b = c;
+
+a = a + b; // 5 + 6 = 11
+b = a - b; // 11 - 6 = 5
+a = a - b; // 11 - 5 = 6
+
+console.log(`a is ${a} and b is ${b}`);
