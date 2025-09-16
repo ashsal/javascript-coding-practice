9d6174bda9c74573117e945afaf8f7fa854a1ede
Ashish Saluja <ashishsaluja1@gmail.com>
2025-09-16 10:49:45 +0400

    Fizz buzz

---
+++ FizzBuzz.js
@@ -0,0 +1,17 @@
+function fizzBuzz(inputNumber) {
+  // validate that inputNumber is positive integer number
+  const result = "";
+  for (let i = 1; i <= inputNumber; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+fizzBuzz(36);
